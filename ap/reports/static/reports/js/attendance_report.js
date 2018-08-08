d32919455457f46a9391fffa7d7e18d62f02e1e6
iamsitting <cdsalamanca27@gmail.com>
2018-08-08 10:29:16 -0700

    address codacy a little bit, delete commented code

--- ap/reports/static/reports/js/attendance_report.js
+++ ap/reports/static/reports/js/attendance_report.js
@@ -8,45 +8,14 @@ function getAverageValues(){
   return returnAverageValues;
 }
 
-function getAttendanceRecord(ids, url){
-  traineeIds = ids;
-
-  // define headers and computed values to calculate averages
-  for (let i =0; i<averageHeaders.length; i++){
-    averageValues.push(0);
-  }
-
-  traineeIds.forEach(function(element) {
-    getTraineeRecord(element, url);
-  });
-}
-
 // dynamically adjust the progress bar as ajax requets are being completed
 function updateProgress(){
   const computedPercentage = Math.round(attendanceRecords.length / traineeIds.length * 100);
   $("#progressbar").children().attr("aria-valuenow", computedPercentage).css("width", computedPercentage + "%").text(computedPercentage + "% Complete");
 }
 
-// making a a single row record of HTML to append first for localities by listing only the serving teams
-function appendResponse(traineeInfo){
-  let traineeRecord = document.createElement("tr");
-  const columnHeaders = ["name", "ta", "term", "team", "gender", "unexcused_absences_percentage", "tardy_percentage", "sickness_percentage", "classes_missed_percentage"]
-
-  for (let i = 0; i< columnHeaders.length; i++){
-    let col = document.createElement("td");
-    col.innerHTML = traineeInfo[columnHeaders[i]];
-    traineeRecord.append(col);
-  }
-
-  const tableId = "locality-" + traineeInfo["sending_locality"];
-  $("#"+tableId).children("tbody").append(traineeRecord);
-
-  append_team(traineeRecord.cloneNode(true), traineeInfo);
-
-}
-
 // adjust the record made for localities and adjust it for teams by changing the serving teams to sending localities
-function append_team(constructedRow, traineeInfo){
+function appendTeam(constructedRow, traineeInfo){
   const locId = traineeInfo["sending_locality"];
   const locality = $("#locality-"+locId).prev().children().first().text();
   let col = constructedRow.children.item(3);
@@ -57,7 +26,22 @@ function append_team(constructedRow, traineeInfo){
 
 }
 
-function sendAverages(){
+// making a a single row record of HTML to append first for localities by listing only the serving teams
+function appendResponse(traineeInfo){
+  let traineeRecord = document.createElement("tr");
+  const columnHeaders = ["name", "ta", "term", "team", "gender", "unexcused_absences_percentage", "tardy_percentage", "sickness_percentage", "classes_missed_percentage"];
+
+  for (let i = 0; i< columnHeaders.length; i++){
+    let col = document.createElement("td");
+    col.innerHTML = traineeInfo[columnHeaders[i]];
+    traineeRecord.append(col);
+  }
+
+  const tableId = "locality-" + traineeInfo["sending_locality"];
+  $("#"+tableId).children("tbody").append(traineeRecord);
+
+  appendTeam(traineeRecord.cloneNode(true), traineeInfo);
+
 }
 
 // single ajax request for a single trainee based upon trainee id
@@ -81,7 +65,7 @@ function getTraineeRecord(traineeId, url){
 
           // once all the ajax requests are completed, compute the averages and render it
           // also show the content of the now completed attendance report
-          if (attendanceRecords.length == traineeIds.length){
+          if (attendanceRecords.length === traineeIds.length){
             let list = document.createElement("ul");
 
             for (let i = 0; i < averageHeaders.length; i++) {
@@ -94,13 +78,24 @@ function getTraineeRecord(traineeId, url){
             };
 
             $("#averages").append(list);
-            sendAverages();
 
-            console.log("complete");
             $("#navigation_bar").show();
             $(".tab-content").show();
             $(".progress-bar").removeClass("active");
           }
         },
-      })
-};
+      });
+}
+
+function getAttendanceRecord(ids, url){
+  traineeIds = ids;
+
+  // define headers and computed values to calculate averages
+  for (let i =0; i<averageHeaders.length; i++){
+    averageValues.push(0);
+  }
+
+  traineeIds.forEach(function(element) {
+    getTraineeRecord(element, url);
+  });
+}
