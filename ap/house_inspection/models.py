b45165b09bdf68c5a7767bf55889610a8e1b8931
AngeloBelUCSD <abelmont@ucsd.edu>
2019-02-25 17:56:15 -0800

    Fixed EOF error

--- ap/house_inspection/models.py
+++ ap/house_inspection/models.py
@@ -65,32 +65,3 @@ class InspectableHouses(models.Model):
 	uninspectable = models.BooleanField(default=False)
 	def __str__(self):
 		return '%s' % (self.residence)
-
-
-'''
-class Listing(models.Model):
-	realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
-	title = models.CharField(max_length=200)
-	address = models.CharField(max_length=200)
-	city = models.CharField(max_length=100)
-	state = models.CharField(max_length=100)
-	zipcode = models.CharField(max_length=20)
-	description = models.TextField(blank=True) # this means it is optional.
-	price = models.IntegerField()
-	bedrooms = models.IntegerField()
-	bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
-	garage = models.IntegerField(default=0)
-	sq_ft = models.IntegerField()
-	lot_size = models.DecimalField(max_digits=5, decimal_places=1)
-	photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/') # define where it gets uploaded to. 
-	photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-	photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-	photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-	photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-	photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-	photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-	is_published = models.BooleanField(default=True)
-	list_date = models.DateTimeField(default=datetime.now, blank=True)
-	# we need a main field to be displayed.
-	def __str__(self):
-		return self.title
