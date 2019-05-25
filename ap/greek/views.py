f6efba082e8de760057dc04d659b6ab2e86935e6
Kimberly Seria <kimberly@bitbucket.com>
2019-05-25 15:48:23 -0700

    greek vocab - sort by chapter

--- ap/greek/views.py
+++ ap/greek/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from django.http import HttpResponse
 from .models import Vocab
 
 def index(request):
 
+    # Vocab is filtered by chapter
+    chapters = []
+    for c in range(1, 17):
+        chapters.append(c)
+
     greek_list = []
     english_list = []
 
-    # greekVocab = list(Vocab.greek)
-    # print greekVocab
-    for e in Vocab.objects.all():
-        greek_list.append(e)
-        # print(e.greek)
+    greek_list = list(Vocab.objects.filter(chapter="1"))
+    print greek_list
 
     context={
-        "greekVocab": list(greek_list),
+        "chapters": chapters,
+        "greekVocab": greek_list,
+        "english": english_list,
     }
 
     return render(request, 'greek_helper/vocab_list.html', context=context)
