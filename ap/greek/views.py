81bb8a9bbfd5780858d68a9fef7ee9c78fd17cf1
Kimberly Seria <kimberly@bitbucket.com>
2019-06-08 17:03:48 -0700

    greek index page

--- ap/greek/views.py
+++ ap/greek/views.py
@@ -5,31 +5,20 @@ from .models import Vocab
 
 def index(request):
 
-    # Vocab is filtered by chapter
+    greek_list = []
+    english_list = []
+
+    # By default, Greek page loads vocabs in chapter 1
+    greek_list = Vocab.objects.filter(chapter=1)
+
+    # Vocab landing page is filtered by chapter
     chapters = []
     for c in range(1, 17):
         chapters.append(c)
 
-    chap = 1
-    greek_list = []
-    english_list = []
-
-    # The greek helper page will initially load chapter 1
-    greek_list = list(Vocab.objects.all().filter(chapter=chap))
-    print greek_list
-
-    greek = Vocab.objects.all()
-    eng = []
-    for g in greek:
-        e = VocabForm(instance = g)
-        eng.append({'id': g.id, 'form': form})
-
-    print eng
-
     context={
         "chapters": chapters,
-        "greekVocab": list(greek_list),
-        "english": english_list,
+        "greekVocab": greek_list,
     }
 
     return render(request, 'greek_helper/vocab_list.html', context=context)
