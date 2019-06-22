145556d2f11e32c46b7c5b5b0831c6a30b442029
zioninzion <zionpark93@gmail.com>
2019-06-22 15:35:02 -0700

    message-model

--- ap/messaging/models.py
+++ ap/messaging/models.py
@@ -7,3 +7,7 @@ from accounts.models import User
 # Create your models here.
 class Conversation(models.Model):
   participants = models.ManyToManyField(User, null=True, blank=True)
+  messages = models.ManyToManyField(Message, null=True, blank=True)
+
+class Message(models.Model):
+  text = models.ForeignKey(User, null=True, blank=True)
