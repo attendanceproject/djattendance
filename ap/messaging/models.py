8f1f01c0de6888c3cad6c8da55b88cd37e762f5b
zioninzion <zionpark93@gmail.com>
2019-06-27 13:51:10 -0700

    related-name

--- ap/messaging/models.py
+++ ap/messaging/models.py
@@ -5,9 +5,12 @@ from django.db import models
 from accounts.models import User
 
 # Create your models here.
+class Message(models.Model):
+  text = models.TextField(null=True, blank=True)
+  author = models.ForeignKey(User, null=True, blank=True, related_name='wrote')
+  read = models.ManyToManyField(User, null=True, blank=True, related_name='read')
+
 class Conversation(models.Model):
   participants = models.ManyToManyField(User, null=True, blank=True)
   messages = models.ManyToManyField(Message, null=True, blank=True)
 
-class Message(models.Model):
-  text = models.ForeignKey(User, null=True, blank=True)
