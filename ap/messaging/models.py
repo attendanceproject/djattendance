b501dc1adba22d0a6c43f4ce4c147b76528c9df0
zioninzion <zionpark93@gmail.com>
2019-06-27 17:40:43 -0700

    model-change

--- ap/messaging/models.py
+++ ap/messaging/models.py
@@ -7,10 +7,23 @@ from accounts.models import User
 # Create your models here.
 class Message(models.Model):
   text = models.TextField(null=True, blank=True)
-  author = models.ForeignKey(User, null=True, blank=True, related_name='wrote')
-  read = models.ManyToManyField(User, null=True, blank=True, related_name='read')
+  sent_by = models.ForeignKey(User, null=True, blank=True)
+  read_by = models.ManyToManyField(User, null=True, blank=True)
+  time_sent = models.DateTimeField(editable=False)
+
+  def save(self, *args, **kwargs):
+    if not self.id:
+      self.time_sent = timezone.now()
+    return super(User, self).save(*args, **kwargs)
 
 class Conversation(models.Model):
   participants = models.ManyToManyField(User, null=True, blank=True)
   messages = models.ManyToManyField(Message, null=True, blank=True)
+  time_created = models.DateTimeField(editable=False)
+
+  def save(self, *args, **kwargs):
+    if not self.id:
+      self.time_created = timezone.now()
+    return super(User, self).save(*args, **kwargs)
+
 
