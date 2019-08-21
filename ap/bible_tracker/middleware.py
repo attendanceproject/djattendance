f232d58381a21dd6fe39036c9ec66a25d522156c
Peter Watson <plew.watson@outlook.com>
2019-08-21 07:40:46 -0700

    Temp Fix to remove force page

--- ap/bible_tracker/middleware.py
+++ ap/bible_tracker/middleware.py
@@ -19,5 +19,6 @@ class BibleReadingMiddleware(MiddlewareMixin):
     if request.path not in url_list:
       week = unfinalized_week(request.user)
       if week and not settings.DEBUG:
-        return HttpResponseRedirect(reverse('bible_tracker:index') + '?week=' + str(week))
+        return None
+        #return HttpResponseRedirect(reverse('bible_tracker:index') + '?week=' + str(week))
     return None
